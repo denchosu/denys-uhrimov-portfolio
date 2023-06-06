cffc52a1187a73d70a3ab1bd7bc4e5fa49f74896
denchosu <mbdench@gmail.com>
2023-06-06 19:38:14 +0300

    i forgor

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import image1 from './images/image-1.png'
 export default function Home() {
   return (<><Image id={styles.pageimg} alt='img' src={image1}></Image>
   <div id={styles.pagetxt}>
-   <p>Hey im a professional slacker!! I love playing videogames and drinking beer :33</p>
+   <p>Hey my name is Denys Uhrimov and im a professional slacker!! I love playing videogames and drinking beer :33</p>
   
   </div>
 </>)
